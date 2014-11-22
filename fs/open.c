24a7a366f031e983a7e32708c682b837a5e763c4
David Ward <david.ward@ll.mit.edu>
2014-11-22 14:47:02 -0500

    Revert extra modifications to source tree
    This reverts modifications to the Linux kernel stable source tree
    (git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git)
    except those which implement CSI Tool support. The CSI Tool README
    is renamed to avoid conflicting with the Linux kernel release notes
    (but it will still appear as the markdown in GitHub).
    
    This makes it easier to merge this tree with distribution-specific
    kernel changes, or to rebase onto newer kernel releases.
    
    Signed-off-by: David Ward <david.ward@ll.mit.edu>

--- fs/open.c
+++ fs/open.c
@@ -33,9 +33,6 @@
 
 #include "internal.h"
 
-#define CREATE_TRACE_POINTS
-#include <trace/events/fs.h>
-
 int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
 	struct file *filp)
 {
@@ -990,7 +987,6 @@ long do_sys_open(int dfd, const char __user *filename, int flags, int mode)
 			} else {
 				fsnotify_open(f);
 				fd_install(fd, f);
-				trace_do_sys_open(tmp, flags, mode);
 			}
 		}
 		putname(tmp);
